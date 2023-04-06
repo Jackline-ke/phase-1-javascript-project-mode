52fda448dbafe6e4294e35d912a465cce003dc1c
Jackline-ke <antoninahodhiambo@gmail.com>
2023-04-06 11:12:13 +0300

    CSS file

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const api_url = base_url + '/discover/movie?sort_by=popularity.desc&' + ;
 */
 
 const main = document.createElement('main')
+main.className = 'movies_body'
 
 //fetch our data from the api
 function getData(){
@@ -14,11 +15,31 @@ function getData(){
     .then(response => response.json())
     .then(data => {
 
-        data.forEach(movie => {
+        data.forEach(movies => {
             //create ,div,img,h3,p
 
-            con
+            const divMovies = document.createElement('div');
+            divMovies.className = 'movies';
+            const image = document.createElement('img');
+            image.class = 'movie_image';
+            const divMoviesInformation = document.createElement('div');
+            divMoviesInformation.class = 'movies_info';
+            const title = document.createElement('h4');
+            title.className = 'title';
+            const rate = document.createElement('span');
+            const overview = document.createElement('div');
+            overview.className = 'overview';
+
+            //assign values
+            image.src = `${movies.poster_path}`;
+            title.textContent =`${movies.title}`;
+            rate.textContent =`${movies.vote_average}`;
+            overview.textContent = `${movies.overview}`;
+
+            //append the child element to the parent element
+            
+
             
         });
     })
-}
+};
